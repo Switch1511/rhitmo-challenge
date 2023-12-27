a0cd1657c19bff3d3839f652386da74d8e326b5f
Switch1511 <gustavopasqualli0@gmail.com>
2023-12-27 00:49:03 -0300

    feat: Created files components

---
+++ src/app/core/view/components/breadcumb/breadcumb.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BreadcumbComponent } from './breadcumb.component';
+
+
+
+@NgModule({
+  declarations: [
+    BreadcumbComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class BreadcumbModule { }
